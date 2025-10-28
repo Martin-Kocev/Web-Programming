67692918792cf0a4d9418f2a9c1c21885b77450d
MaKo-21 <kocev.martin3@gmail.com>
2025-10-28 01:50:39 +0100

    Testing if parameters get passed when forwarding

---
+++ src/main/java/mk/ukim/finki/wp/wp2025/web/servlet/TestSender.java
@@ -0,0 +1,46 @@
+package mk.ukim.finki.wp.wp2025.web.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+
+@WebServlet(name = "TestSender", urlPatterns = {"", "/test"})
+public class TestSender extends HttpServlet {
+
+    private final SpringTemplateEngine templateEngine;
+
+    public TestSender(SpringTemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        IWebExchange webExchange = JakartaServletWebApplication
+                .buildApplication(req.getServletContext())
+                .buildExchange(req, resp);
+
+        WebContext context = new WebContext(webExchange);
+
+        templateEngine.process("test.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        IWebExchange webExchange = JakartaServletWebApplication
+                .buildApplication(req.getServletContext())
+                .buildExchange(req, resp);
+
+        WebContext context = new WebContext(webExchange);
+
+        req.getRequestDispatcher("servlet/TestReceiver.java").forward(req, resp);
+    }
+}
